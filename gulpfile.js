8f49fe3bb535f26d39a095e6e5fe9db5ae3a7ebc
jacob <jacob.patrick.harris@gmail.com>
2017-06-06 10:19:17 -0400

    renaming things internally

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('styles', function() {
-    gulp.src('scss/bicep.scss')
+    gulp.src('scss/lfg.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
           browsers: ['last 2 versions'],
           cascade: false
         }))
         .pipe(minifyCSS({compatibility: 'ie8'}))
-        .pipe(rename('bicep.min.css'))
+        .pipe(rename('lfg.min.css'))
         .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default',function() {
-    gulp.watch('scss/bicep.scss',['styles']);
+    gulp.watch('scss/lfg.scss',['styles']);
 });
 
 
